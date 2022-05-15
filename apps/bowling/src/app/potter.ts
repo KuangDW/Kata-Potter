0ac30f03c78ab6ddef9c0f03574a9cd4ac78265b
KuangDW <max9191327220@gmail.com>
2022-05-16 00:45:53 +0800

    test2 commit

--- apps/bowling/src/app/potter.ts
+++ apps/bowling/src/app/potter.ts
@@ -1,14 +1,57 @@
 class Book {
-    constructor(protected bookNumber: number) {}
+    constructor(public number: number) {}
 }
-
 export class Potter {
+    price = 8;
+    discounts = [0, 0.05, 0.1, 0.2, 0.25];
     basket: Book[] = [];
 
-    price = 8;
-
     checkout(): number {
-        return this.price * this.basket.length
+        const qtyOfPacksBySize = this.sortBooksInPacks(
+            this.sortBooksByQtyOfRepeatingNumbers()
+        );
+        let total = 0;
+
+        for (let pack = 0; pack < qtyOfPacksBySize.length; pack++) {
+            if (qtyOfPacksBySize[pack] > 0) {
+                let numberOfBooksInPack = pack + 1;
+
+                let applicableDiscount = this.discounts[pack];
+
+                let subtotal = numberOfBooksInPack * this.price;
+
+                let discountedPriceForPack =
+                    subtotal - subtotal * applicableDiscount;
+
+                total += qtyOfPacksBySize[pack] * discountedPriceForPack;
+            }
+        }
+        return total;
+    }
+
+    sortBooksInPacks(booksByQtyOfNumbers: number[]): number[] {
+        let packs: number[] = new Array(this.discounts.length + 1).fill(0);
+
+        for (let qty = 0; qty < booksByQtyOfNumbers.length; qty++) {
+            if (booksByQtyOfNumbers[qty] > 0) {
+                packs[qty] = booksByQtyOfNumbers[qty];
+                booksByQtyOfNumbers = booksByQtyOfNumbers.map((q) => {
+                    return q > 0 ? q - booksByQtyOfNumbers[qty] : q;
+                });
+            }
+        }
+        return packs.reverse();
+    }
+
+    sortBooksByQtyOfRepeatingNumbers(): number[] {
+        let qtyOfBooksByNumber: number[] = new Array(
+            this.discounts.length + 1
+        ).fill(0);
+
+        this.basket.forEach((b) => {
+            qtyOfBooksByNumber[b.number] += 1;
+        });
+        return qtyOfBooksByNumber.sort();
     }
 
     createBook(bookNumber: number): Book {
