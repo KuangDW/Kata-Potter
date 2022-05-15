380c6fa4e612b073fff761ffb139d75ebf7285ea
KuangDW <max9191327220@gmail.com>
2022-05-16 00:45:34 +0800

    test2 commit

--- apps/bowling/src/app/potter.spec.ts
+++ apps/bowling/src/app/potter.spec.ts
@@ -15,4 +15,21 @@ describe('Harry Potter book kata', function() {
     expect(potter.createBook(number)).toMatchObject({ number });
   });
 
+  test('...the addToBasket method should add books to the shopping basket', function() {
+    const potter = new Potter();
+    const book = potter.createBook(1);
+    potter.addToBasket(book);
+    expect(potter.basket).toHaveLength(1);
+  });
+
+  test('...x number of the same books should result in base price * x', function() {
+    const potter = new Potter();
+    const book = potter.createBook(1);
+    potter.addToBasket(book);
+    potter.addToBasket(book);
+    expect(potter.checkout()).toBe(16);
+    potter.addToBasket(book);
+    expect(potter.checkout()).toBe(24);
+  });
+
 });
